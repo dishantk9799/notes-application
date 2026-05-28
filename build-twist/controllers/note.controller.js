8a632317507bfb653e100ccd504ced7d9850ca13
Dishant kumawat <dishantk9799@gmail.com>
2026-05-28 10:49:15 +0530

    build-twist error handle in controllers

--- build-twist/controllers/note.controller.js
+++ build-twist/controllers/note.controller.js
@@ -1,35 +1,47 @@
 // ---- Create note ----
-export const createNote=async(req,res)=>{
+export const createNote = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+        console.log("Error in Create note:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 };
 
 // ---- Read note ----
-export const readNote=async(req,res)=>{
+export const readNote = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+        console.log("Error in Read note:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 };
 
 // ---- Update note ----
-export const updateNote=async(req,res)=>{
+export const updateNote = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+        console.log("Error in Update note:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 };
 
 // ---- delete note ----
-export const deleteNote=async(req,res)=>{
+export const deleteNote = async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+        console.log("Error in Delete note:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 };
