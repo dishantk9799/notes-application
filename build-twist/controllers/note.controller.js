c3cf99ab981b36c8a33e0c50ed19220d74f83acb
Dishant kumawat <dishantk9799@gmail.com>
2026-05-28 10:51:48 +0530

    build-twist add controller create note

--- build-twist/controllers/note.controller.js
+++ build-twist/controllers/note.controller.js
@@ -1,6 +1,24 @@
+import { Note } from "../models/note.model.js";
+
 // ---- Create note ----
 export const createNote = async (req, res) => {
     try {
+        // ---- User provided data ----
+        const { title, description } = req.body;
+
+        // ---- Validation ----
+        if (!title) return res.status(400).json({ message: "title is required" });
+        if (!description) return res.status(400).json({ message: "description is required" });
+        if (title.trim().length < 3) return res.status(400).json({ message: "title should be more then 3 character" });
+        if (description.trim().length < 10) return res.status(400).json({ message: "description should be more then 10 character" });
+
+        // ---- Creating note ----
+        const newNote = await Note.create({ title, description });
+
+        return res.status(201).json({
+            message: "Note created successfully",
+            note: newNote
+        });
 
     } catch (error) {
         console.log("Error in Create note:", error);
