2eaf2723625498be8fe8e506dcb99cc06d3f9eec
Dishant kumawat <dishantk9799@gmail.com>
2026-05-28 10:45:47 +0530

    build-twist controller basic setup

---
+++ build-twist/controllers/note.controller.js
@@ -0,0 +1,35 @@
+// ---- Create note ----
+export const createNote=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// ---- Read note ----
+export const readNote=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// ---- Update note ----
+export const updateNote=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// ---- delete note ----
+export const deleteNote=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
--- build-twist/routes/note.route.js
+++ build-twist/routes/note.route.js
@@ -1,25 +1,18 @@
 import express from 'express';
+import { createNote, deleteNote, readNote, updateNote } from '../controllers/note.controller.js';
 
 const route = express.Router();
 
 // ---- Create note route ----
-route.post('/notes', (req, res) => {
-
-});
+route.post('/notes', createNote);
 
 // ---- Read note route ----
-route.get('/notes', (req, res) => {
-
-});
+route.get('/notes', readNote);
 
 // ---- Update note route ----
-route.patch('/notes/:id', (req, res) => {
-
-});
+route.patch('/notes/:id', updateNote);
 
 // ---- Delete note route ----
-route.delete('/notes/:id', (req, res) => {
-
-});
+route.delete('/notes/:id', deleteNote);
 
 export default route;
